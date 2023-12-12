3cb930f2ad5c2682c466273d551f6d31176787a5
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2023-12-12 12:10:51 +0100

    Handle the exit command and whitespaces

--- cc_input_exit.c
+++ cc_input_exit.c
@@ -2,7 +2,7 @@
 
 /**
  * cc_input_exit - Removes '\n' from input string and
- * checks if the input is "exit" or "EOF".
+ *                 checks if the input is "exit" or "EOF".
  * @i_b: The buffer that holds the input from user.
  * @b_r: The length of the input from user.
  *
@@ -10,12 +10,24 @@
  */
 void cc_input_exit(char *i_b, ssize_t b_r)
 {
-	int i;
+	int i = 0;
+
+	while (i_b[0] == ' ')
+		i_b++; /* Remove leading white spaces */
 
 	for (i = 0; i_b[i]; i++)
 		if (i_b[i] == '\n')
 			i_b[i] = '\0';
 
-	if (strcmp(i_b, "exit") == 0 || b_r == 0)
+	for (i = i - 2; i_b[i] == ' '; i--)
+		i_b[i] = '\0'; /* Remove trailing whitespaces */
+
+	if (strcmp(i_b, "exit") == 0)
 		exit(0);
+
+	if (b_r == 0)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		exit(0);
+	}
 }
