9852a487a5accfe67a9fe89d57fc3056886dfe9c
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2023-12-13 07:04:28 +0100

    Betty errors resolved

--- main.h
+++ main.h
@@ -14,8 +14,7 @@ char *_strtok(char *str, const char *delim);
 void handle_error(int err_value);
 int _strlen(char *str);
 void cc_input_exit(char *i_b, ssize_t b_r);
-/*void clean_input(char *input_buf);*/
-int loop_for(char *str, char delim); /*Currently in is_path.c, change later to tokenize.c*/
+int loop_for(char *str, char delim);
 char **tokenize(char *input_buf);
 int is_valid_path(char *command, char **path, char **env);
 void sfree_memory(char *mem_address);
