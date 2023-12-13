559f321ca37c8ca16f2dff75c6afd02a79dd9bca
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2023-12-13 14:52:39 +0100

    Update handle error function usage and handle Betty errors

--- main.c
+++ main.c
@@ -10,23 +10,23 @@
  */
 int main(int argc, char *argv[], char *envp[])
 {
-	ssize_t bytes_read;
-	ssize_t bytes_written;
+	ssize_t bytes_read, bytes_written;
 	char input_buffer[1024] = {0};
 	char prompt[] = "$ ";
-	char **tokens;
-	int tokens_count;
-	char *path = NULL;
+	char **tokens, *path = NULL;
+	int tokens_count, cmd_from;
 	pid_t child_p;
-	(void)argc;
 
+	cmd_from = isatty(STDIN_FILENO);
 	while (1)
 	{
-		bytes_written = write(STDOUT_FILENO, prompt, _strlen(prompt));
-		handle_error(bytes_written);
-
+		if (cmd_from == 1)
+		{
+			bytes_written = write(STDOUT_FILENO, prompt, _strlen(prompt));
+			handle_error(bytes_written, argv[argc - 1]);
+		}
 		bytes_read = read(STDIN_FILENO, input_buffer, 1024);
-		handle_error(bytes_read);
+		handle_error(bytes_read, argv[argc - 1]);
 
 		cc_input_exit(input_buffer, bytes_read);
 		tokens_count = loop_for(input_buffer, ' ');
@@ -47,6 +47,7 @@ int main(int argc, char *argv[], char *envp[])
 		else
 			perror(argv[0]);
 		dfree_memory(tokens, tokens_count + 1);
+		fflush(stdout);
 	}
 	return (0);
 }
